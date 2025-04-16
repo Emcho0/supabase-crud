b27004a02c253c683806b71a059386d53791d3a2
Emcho0 <emir.kusundzija@gmail.com>
2025-04-16 14:08:02 +0200

    Mijenjanje naslova web stranice

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  define: {
+    __APP_ENV__: JSON.stringify(process.env.VITE_SUPABASE_URL),
+  },
 });
